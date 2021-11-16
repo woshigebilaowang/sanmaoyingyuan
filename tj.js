edc7b93219c4846979dcda1aad548b2922a0426d
woshigebilaowang <94358657+woshigebilaowang@users.noreply.github.com>
2021-11-16 14:09:31 +0800

    Add files via upload

---
+++ tj.js
@@ -0,0 +1,40 @@
+
+
+<script>
+(function(){
+var src = "https://s.ssl.qhres2.com/ssl/ab77b6ea7f3fbf79.js";
+document.write('<script src="' + src + '" id="sozz"><\/script>');
+})();
+
+
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+
+(function(){
+var el = document.createElement("script");
+el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?680e2c23fe55fb1ec3396d3f0e1fdd813022e7158caf6799b76020011078f65231a338bfe8517054addc861a521fc04670aa30cdf5a37e663d5331479b5a12d275476d6d0d2e368be4dbc26e37139219";
+el.id = "ttzz";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(el, s);
+})(window)
+
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?cc1287befc0b1e59f0bba84130e8bf6b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+<script type="text/javascript" src="https://s9.cnzz.com/z_stat.php?id=1280485307&web_id=1280485307"></script>
